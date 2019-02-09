f40961385c9af3a253dca7054c75082a0d17ffa8
indraarianggi <indraarianggi@gmail.com>
2019-02-09 19:19:04 +0700

    delete profile and user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -308,4 +308,21 @@ router.delete(
     }
 );
 
+/**
+ * @route           DELETE api/profile
+ * @description     Delete user and profile
+ * @access          Private
+ */
+router.delete(
+    "/",
+    passport.authenticate("jwt", { session: false }),
+    (req, res) => {
+        Profile.findOneAndRemove({ user: req.user.id }).then(() => {
+            User.findOneAndRemove({ _id: req.user.id }).then(() =>
+                res.json({ success: true })
+            );
+        });
+    }
+);
+
 module.exports = router;
