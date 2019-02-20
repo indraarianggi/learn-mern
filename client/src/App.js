25787df1dd020f723dd14dee1b422629407149b4
indraarianggi <indraarianggi@gmail.com>
2019-02-20 07:23:28 +0700

    redux, react-redux in login system

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser } from "./actions/authAction";
 
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
@@ -10,6 +13,18 @@ import { Provider } from "react-redux";
 import store from "./store";
 
 import "./App.css";
+
+// Check for token
+if (localStorage.jwtToken) {
+    // Set auth token header auth
+    setAuthToken(localStorage.jwtToken);
+
+    // Decode token and get user info and exp
+    const decoded = jwt_decode(localStorage.jwtToken);
+
+    // Set user and isAuthenticated
+    store.dispatch(setCurrentUser(decoded));
+}
 
 class App extends Component {
     render() {
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,5 +1,8 @@
 // Reducer #1: auth
 
+import isEmpty from "../validations/is-empty";
+import { SET_CURRENT_USER } from "../actions/types";
+
 const initialState = {
     isAuthenticated: false,
     user: {}
@@ -7,6 +10,12 @@ const initialState = {
 
 export default function(state = initialState, action) {
     switch (action.type) {
+        case SET_CURRENT_USER:
+            return {
+                ...state,
+                isAuthenticated: !isEmpty(action.payload),
+                user: action.payload
+            };
         default:
             return state;
     }
