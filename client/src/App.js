1ea1586e39ad9e712aa286781f3a20a958fdbf07
indraarianggi <indraarianggi@gmail.com>
2019-03-05 04:29:45 +0700

    input group component and create profile form component

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Dashboard from "./components/dashboard/Dashboard";
+import CreateProfile from "./components/create-profile/CreateProfile";
 
 import "./App.css";
 
@@ -68,6 +69,14 @@ class App extends Component {
                                     exact
                                     path="/dashboard"
                                     component={Dashboard}
+                                />
+                            </Switch>
+
+                            <Switch>
+                                <PrivateRoute
+                                    exact
+                                    path="/create-profile"
+                                    component={CreateProfile}
                                 />
                             </Switch>
                         </div>
