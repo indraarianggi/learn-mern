c7567327db39354756183ca8ba61b5a6ac4713e2
indraarianggi <indraarianggi@gmail.com>
2019-03-09 07:03:14 +0700

    add experience and education

--- client/src/actions/profileAction.js
+++ client/src/actions/profileAction.js
@@ -28,6 +28,32 @@ export const getCurrentProfile = () => dispatch => {
         );
 };
 
+// Add experience
+export const addExperience = (expData, history) => dispatch => {
+    axios
+        .post("/api/profile/experience", expData)
+        .then(res => history.push("/dashboard"))
+        .catch(err =>
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        );
+};
+
+// Add education
+export const addEducation = (eduData, history) => dispatch => {
+    axios
+        .post("/api/profile/education", eduData)
+        .then(res => history.push("/dashboard"))
+        .catch(err =>
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        );
+};
+
 // Create Profile
 export const createProfile = (profileData, history) => dispatch => {
     axios
